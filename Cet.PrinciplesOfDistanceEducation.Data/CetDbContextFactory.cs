e615ac295abd6c497e223d9c1cdce6126d71fa72
suddenly30 <xtinasfighters@gmail.com>
2019-05-12 12:17:07 +0300

    Update CetDbContextFactory.cs

--- Cet.PrinciplesOfDistanceEducation.Data/CetDbContextFactory.cs
+++ Cet.PrinciplesOfDistanceEducation.Data/CetDbContextFactory.cs
@@ -6,7 +6,7 @@ namespace Cet.PrinciplesOfDistanceEducation.Data
 {
     public class CetDbContextFactory: IDesignTimeDbContextFactory<CetDbContext>
     {
-        // Server=193.140.200.25;Database=cet441demo_db;User Id=cet441demo_user;Password=c3VwZXJ1c2Vy;
+        
         public CetDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<CetDbContext>();
